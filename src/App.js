f12244b5a52814d7a24c8b2a63a2d91790ebdda2
MrMisterBean <alutter03@gmail.com>
2022-12-30 00:43:20 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className='Header'>
+      <div className='px-2 py-8 text-4xl text-right text-white bg-black font-spaghettica'>
+        Spaghetti Dan
+      </div>
+      <div className='Hamburger'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
