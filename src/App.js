c79f7dc14f619173c589a0b40dbdbbcda3ca0467
MrMisterBean <alutter03@gmail.com>
2022-12-30 21:07:25 -0500

    added hamburger button and icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './index.css';
+import burger from './images/burger_white.png';
 
 function App() {
   return (
-    <div className='Header'>
-      <div className='px-2 py-8 text-4xl text-right text-white bg-black font-spaghettica'>
-        Spaghetti Dan
+    <div className='flex justify-around px-2 py-8 text-4xl text-right text-white bg-black font-spaghettica'>
+      <div className='flex items-center'>
+        <button><img src={burger} alt='hamburger' className='w-6 h-6' /></button>
       </div>
-      <div className='Hamburger'>
-
+      <div>
+        Spaghetti Dan
       </div>
     </div>
   );
