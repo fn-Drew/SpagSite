d4d04762a54a7e774974838662d7f5e3178f95de
MrMisterBean <alutter03@gmail.com>
2022-12-31 23:06:14 -0500

    added image into sidebar and image  gallery on main page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
+import chainz from './images/chainz.png'
 import burger from './images/burger_white.png'
+import flannel from './images/flannel.png'
+import sleep from './images/sleep_upscaled.png'
+import earbert from './images/earbert_slim.png'
+import dwayne from './images/dwayne.png'
+import pain from './images/pain.png'
 import { useState } from 'react';
 
 const App = () => {
-
-  // const [showSidebar, setShowSidebar] = useState(false);
-  // return (
-  //   <div className='flex justify-around px-2 py-8 text-4xl text-right text-white duration-500 ease-in-out bg-black font-spaghettica'>
-  //     {showSidebar ? (
-  //       <div className='flex items-center '>
-  //         <button onClick={() => setShowSidebar(!showSidebar)}><img src={burger} alt='hamburger' className='w-6 h-6' /></button>
-  //       </div>
-  //     ) : (
-  //       <div className='flex items-center'>
-  //         <button className="z-50" onClick={() => setShowSidebar(!showSidebar)}><img src={burger} alt='hamburger' className='w-6 h-6' /></button>
-  //         <div className="fixed grid justify-evenly top-0 left-0 z-40 h-full p-10 pl-20 text-white  bg-black w-[80%]">
-  //           <h3 className="mt-20 text-4xl font-semibold text-white">Discord</h3>
-  //           <h3 className="mt-20 text-4xl font-semibold text-white">Twitter</h3>
-  //           <h3 className="mt-20 text-4xl font-semibold text-white">Donate</h3>
-  //         </div>
-  //       </div>
-  //     )}
-  //     <div>
-  //       Spaghetti Dan
-  //     </div>
-  //   </div>
-  // )
-  //
   const [showSidebar, setShowSidebar] = useState(false);
-  console.log(showSidebar)
+
   return (
     <div className='w-screen h-screen bg-zinc-900'>
 
-      <div className={`flex justify-around px-2 py-8 text-4xl text-right text-white  bg-black font-spaghettica`}>
+      {/* header shadow-lg shadow-zinc-700 */}
+      <div className={`flex justify-around items-center h-[20%] text-center text-4xl text-white bg-black font-spaghettica`}>
         <div className='flex items-center '>
-          <button className='z-20' onClick={() => setShowSidebar(!showSidebar)}><img src={burger} alt='hamburger' className='w-6 h-6' /></button>
+          <button onClick={() => setShowSidebar(!showSidebar)}><img src={burger} alt='hamburger' className='w-6 h-6' /></button>
         </div>
-        <div className='z-20'>
+        <div>
           Spaghetti Dan
         </div>
       </div>
 
+      <div className={`fixed grid bg-slate-black justify-evenly bottom-0 left-0 p-10 pl-20 text-white ease-in-out duration-150 bg-black h-[80%] w-[80%] ${showSidebar ? "translate-x-0" : "translate-x-[-100%]"}`}>
+        <img src={chainz} alt='chainz' className='w-full h-auto' />
+      </div>
+
+      <div className='grid grid-cols-3 text-white inset-4 bg-zinc-800'>
+        <img src={flannel} className='w-full row-span-2' />
+        <img src={sleep} className='w-full col-span-2' />
+        <img src={earbert} className='w-full h-64 col-span-3' />
+        <img src={dwayne} className='w-full col-span-2' />
+        <img src={pain} className='w-full col-span-2' />
+
+      </div>
+
       <div>
-        <div className={`fixed grid z-10 bg-slate-black justify-evenly top-0 left-0 h-full p-10 pl-20 text-white ease-in-out duration-150 ${showSidebar ? "translate-x-0" : "translate-x-[-100%]"} bg-black w-[80%]`}></div>
+
       </div>
 
     </div>
